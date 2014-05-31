380bef545494a9d9e2de3843cf4e7ded3bf9c63c
DeSergo <cer6iy@outlook.com>
2014-05-31 23:52:32 +0700

    Состояние на момент ухода

---
+++ Greenny/Clasterer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Greenny{
+
+    class Point
+    {
+        public const int NOISE = -1;
+        public const int UNCLASSIFIED = 0;
+        public double X, Y;
+        public int ClusterId;
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return String.Format("({0}, {1})", X, Y);
+        }
+        public static double DistanceSquared(Point p1, Point p2)
+        {
+            double diffX = p2.X - p1.X;
+            double diffY = p2.Y - p1.Y;
+            return diffX * diffX + diffY * diffY;
+        }
+    }
+    static class DBSCAN
+    {
+    //    static void Main()
+    //    {
+    //        List<Point> points = new List<Point>();
+    //        // sample data
+    //        //points.Add(new Point(0, 100));
+    //        //points.Add(new Point(0, 200));
+    //        //points.Add(new Point(0, 275));
+    //        //points.Add(new Point(100, 150));
+    //        //points.Add(new Point(200, 100));
+    //        //points.Add(new Point(250, 200));
+    //        //points.Add(new Point(0, 300));
+    //        //points.Add(new Point(100, 200));
+    //        //points.Add(new Point(600, 700));
+    //        //points.Add(new Point(650, 700));
+    //        //points.Add(new Point(675, 700));
+    //        //points.Add(new Point(675, 710));
+    //        //points.Add(new Point(675, 720));
+    //        //points.Add(new Point(50, 400));
+
+    //        DButils a = new DButils();
+    //        a.GetData();
+
+    //        double eps = 100.0;
+    //        int minPts = 3;
+    //        List<List<Point>> clusters = GetClusters(points, eps, minPts);
+            
+    //    }
+        public static List<List<Point>> GetClusters(List<Point> points, double eps, int minPts)
+        {
+            if (points == null) return null;
+            List<List<Point>> clusters = new List<List<Point>>();
+            eps *= eps; // square eps
+            int clusterId = 1;
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point p = points[i];
+                if (p.ClusterId == Point.UNCLASSIFIED)
+                {
+                    if (ExpandCluster(points, p, clusterId, eps, minPts)) clusterId++;
+                }
+            }
+            // sort out points into their clusters, if any
+            int maxClusterId = points.OrderBy(p => p.ClusterId).Last().ClusterId;
+            if (maxClusterId < 1) return clusters; // no clusters, so list is empty
+            for (int i = 0; i < maxClusterId; i++) clusters.Add(new List<Point>());
+            foreach (Point p in points)
+            {
+                if (p.ClusterId > 0) clusters[p.ClusterId - 1].Add(p);
+            }
+            return clusters;
+        }
+        static List<Point> GetRegion(List<Point> points, Point p, double eps)
+        {
+            List<Point> region = new List<Point>();
+            for (int i = 0; i < points.Count; i++)
+            {
+                double distSquared = Point.DistanceSquared(p, points[i]);
+                if (distSquared <= eps) region.Add(points[i]);
+            }
+            return region;
+        }
+        static bool ExpandCluster(List<Point> points, Point p, int clusterId, double eps, int minPts)
+        {
+            List<Point> seeds = GetRegion(points, p, eps);
+            if (seeds.Count < minPts) // no core point
+            {
+                p.ClusterId = Point.NOISE;
+                return false;
+            }
+            else // all points in seeds are density reachable from point 'p'
+            {
+                for (int i = 0; i < seeds.Count; i++) seeds[i].ClusterId = clusterId;
+                seeds.Remove(p);
+                while (seeds.Count > 0)
+                {
+                    Point currentP = seeds[0];
+                    List<Point> result = GetRegion(points, currentP, eps);
+                    if (result.Count >= minPts)
+                    {
+                        for (int i = 0; i < result.Count; i++)
+                        {
+                            Point resultP = result[i];
+                            if (resultP.ClusterId == Point.UNCLASSIFIED || resultP.ClusterId == Point.NOISE)
+                            {
+                                if (resultP.ClusterId == Point.UNCLASSIFIED) seeds.Add(resultP);
+                                resultP.ClusterId = clusterId;
+                            }
+                        }
+                    }
+                    seeds.Remove(currentP);
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Greenny/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using Finisar.SQLite;
+using System.Threading;
+using System.Globalization;
+
+namespace Greenny
+{
+    class DButils
+    {
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        double eps = 0.007;
+        int minPts = 10;
+        List<List<Point>> clusters;
+
+        public List<Point> points = new List<Point>();
+
+        public void PrintClusters()
+        {
+            Console.Clear();
+            // print points to console
+            Console.WriteLine("The {0} points are :\n", points.Count);
+            foreach (Point p in points) Console.Write(" {0} ", p);
+            Console.WriteLine();
+            // print clusters to console
+            int total = 0;
+            for (int i = 0; i < clusters.Count; i++)
+            {
+                int count = clusters[i].Count;
+                total += count;
+                string plural = (count != 1) ? "s" : "";
+                Console.WriteLine("\nCluster {0} consists of the following {1} point{2} :\n", i + 1, count, plural);
+                foreach (Point p in clusters[i]) Console.Write(" {0} ", p);
+                Console.WriteLine();
+            }
+            // print any points which are NOISE
+            total = points.Count - total;
+            if (total > 0)
+            {
+                string plural = (total != 1) ? "s" : "";
+                string verb = (total != 1) ? "are" : "is";
+                Console.WriteLine("\nThe following {0} point{1} {2} NOISE :\n", total, plural, verb);
+                foreach (Point p in points)
+                {
+                    if (p.ClusterId == Point.NOISE) Console.Write(" {0} ", p);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("\nNo points are NOISE");
+            }
+            Console.ReadKey();
+        }
+
+        public void PrintPoints()
+        {
+            if (DT == null) return;
+            Console.WriteLine();
+            foreach (var column in DT.Columns)
+            {
+                DataColumn dataColumn = column as DataColumn;
+
+                Console.Write("{0}", dataColumn.ColumnName + "\t");
+            }
+            Console.WriteLine();
+            StringBuilder str = new StringBuilder();
+            foreach (Point p in points)
+            {
+                str.AppendLine(String.Format("{0}\t{1}", p.X, p.Y));
+
+                //Console.Write("{0} \t\t {1}", p.X, p.Y);
+                //Console.WriteLine();
+            }
+            File.WriteAllText("points.txt", str.ToString());
+        }
+
+        public void ToList()
+        {
+            foreach (DataRow row in DT.Rows)
+            {
+                try
+                {
+                    points.Add(new Point(double.Parse(row.ItemArray[0].ToString().Replace(',', '.')), double.Parse(row.ItemArray[1].ToString().Replace(',', '.'))));
+
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+
+        public void GetData(int b_t = 0, int e_t = 0)
+        {
+
+            sql_con = new SQLiteConnection("Data Source=omsk.sqlite;Version=3;New=False;Compress=True;");
+            sql_con.Open();
+
+            sql_cmd = sql_con.CreateCommand();
+            // подключение геобиблиотеки
+            string query = "SELECT load_extension('libspatialite-2.dll')";
+            SQLiteDataAdapter ret = new SQLiteDataAdapter(query, sql_con);
+
+            string CommandText = "select * from building where Contains(geom, PointFromText(\"POINT(73.365707 54.91741)\"))";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            sql_con.Close();
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            this.ToList();
+            //this.PrintPoints();
+            //Console.ReadKey();
+            clusters = DBSCAN.GetClusters(points, eps, minPts);
+            this.PrintClusters();
+            Console.WriteLine(clusters.Count);
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            DButils a = new DButils();
+            a.GetData();
+            Console.ReadKey();
+        }
+    }
+}
